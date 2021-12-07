4e5d7b80f051ff52b58ccb3efa4ac13e268d8086
Sci4Tech <raannicet1@gmail.com>
2021-12-07 15:57:29 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pluralsight.candycoded;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -74,6 +76,13 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    Intent infoIntent = new Intent(this, InfoActivity.class);
+    startActivity(infoIntent);
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     MenuInflater inflater = getMenuInflater();
     inflater.inflate(R.menu.main, menu);
@@ -96,4 +105,5 @@ public class MainActivity extends AppCompatActivity {
       db.insert(CandyContract.CandyEntry.TABLE_NAME, null, values);
     }
   }
+
 }
